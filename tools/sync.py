d74309b8eac98836379e7a99ca259e6d879d9cfe
Your Name <you@example.com>
2023-09-08 12:50:04 +0800

    增加下载检测功能
    Signed-off-by: Your Name <you@example.com>

--- tools/sync.py
+++ tools/sync.py
@@ -59,6 +59,59 @@ def downsync(ali,local_folder,yun_folder):
     print('\n检查本地文件处理完毕\n')
 
 
+#下载同步，应该以云端为主，将本地没有的文件从云端下载
+def downsynccheck(ali,local_folder,yun_folder):
+    print('\n以云端为主同步文件\n')
+    remote_folder=ali.get_folder_by_path(yun_folder)
+    # 获取remote_folder下的所有文件 # 转为 {文件名:BaseFile对象} 字典模式
+    remote_files={}
+    for f in ali.get_file_list(remote_folder.file_id):
+        remote_file = f.name
+        remote_files[remote_file] = f
+    filecount=len(remote_files)
+    cwd = os.getcwd()
+    curfile=0
+    for f in ali.get_file_list(remote_folder.file_id):
+        remote_file = f.name
+        remote_file_id = f.file_id
+        curfile=curfile+1
+        local_file = os.path.join(local_folder, remote_file)
+        print("checking " + local_file + " " + str(curfile) + "/" + str(filecount))
+        while not os.path.exists(local_file):
+            print("start to download " + remote_file + " " + str(curfile) + "/" + str(filecount))
+            ali.download_file(file=f, local_folder=local_folder)
+            curfile=curfile+1
+            print("download completed")
+            size = os.path.getsize(local_file)
+            if (size < 3000000):
+                print(local_file + " size is too small, delete it and retry")
+                os.remove(local_file)
+                continue
+
+    #ali.sync_folder(local_folder,remote_folder.file_id,False)
+    print('\n云端文件下载完毕\n')
+    # 获取local_folder下的所有文件 # 转为 {文件名:文件路径} 字典模式
+    local_files = {}
+    retry_files = {}
+    for f in os.listdir(local_folder):
+        local_file = os.path.join(local_folder, f)
+        size = os.path.getsize(local_file)
+        if (size < 5000000):
+            os.remove(local_file)
+            retry_files[f]  = local_file
+            continue
+        local_files[f] = local_file
+    # 获取remote_folder下的所有文件 # 转为 {文件名:BaseFile对象} 字典模式
+    remote_files={}
+    for f in ali.get_file_list(remote_folder.file_id):
+        remote_file = f.name
+        remote_files[remote_file] = f
+    print('\n检查本地文件是否需要删除\n')
+    #进行比较,删除本地文件到回收站
+    #for key, value in retry_files.items():
+    print('\n检查本地文件处理完毕\n')
+
+
 def listsync(ali,yun_folder):
     print('\n以云端为主同步文件\n')
     remote_folder=ali.get_folder_by_path(yun_folder)        
@@ -72,6 +125,7 @@ def listsync(ali,yun_folder):
 
 
 if __name__ == '__main__':    
+    print("options: upload download list dlcheck")
     ali = Aligo()    
     print(sys.argv)
     func=sys.argv[1]
@@ -82,5 +136,7 @@ if __name__ == '__main__':
         upsync(ali,local_folder,yun_folder)
     elif func == "download":
         downsync(ali,local_folder,yun_folder)
+    elif func == "dlcheck":
+        downsynccheck(ali,local_folder,yun_folder)
     else:
         listsync(ali,yun_folder)
