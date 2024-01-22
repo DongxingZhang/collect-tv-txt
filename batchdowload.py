ea3a4802675a1adcbe8d42c1d658645b6fbee532
Your Name <you@example.com>
2024-01-22 21:07:39 +0800

    更新20240122
    Signed-off-by: Your Name <you@example.com>

--- batchdowload.py
+++ batchdowload.py
@@ -4,21 +4,11 @@ import sys
 import os
 
 tvlist = [
-    "/短刀行",
-    "/蝉翼传奇AI修复",
-    "/雪山飞狐孟飞",
-    "/射雕英雄传朱茵",
-    "/士兵突击",
-    "/德鲁比",
-    "/拳赌双至尊",
-    "/上海风云",
-    "/洛神",
-    "/龙兄虎弟",
-    "/金剑雕翎AI修复",
-    #"/圆桌派",
-    #"/百家讲坛",
+    ["/洛神","/mnt/share1/movies"],
+    ["/圆桌派","/mnt/share2/movies"],
+    ["/百家讲坛","/mnt/share3/movies"],
 ]
 
 
 for tv in tvlist:
-    os.system("python3 ./tools/sync.py download " + tv + " /mnt/share1/tv" + tv) 
+    os.system("python3 ./tools/sync.py download " + tv[0] + " " + tv[1] + tv[0]) 
