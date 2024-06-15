5be0be2dd51bc703ac70d40853dea57aa8a6e24b
Your Name <you@example.com>
2024-06-15 17:44:34 +0800

    更新最新的代码
    Signed-off-by: Your Name <you@example.com>

--- loop.sh
+++ loop.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 token=$1
 rootdir=`pwd`
+
 echo ${rootdir}
 bash ${rootdir}/${token}.sh bg 750
+
+prev_frame_log=
 while true; do
-    log=$(cat "${rootdir}/log/${token}.log" | grep -a "invalid dropping" | grep -a "DTS" | head -3)
-    echo ===============$log
-    if [ "${log}" != "" ]; then        
+    crash=$(cat "${rootdir}/log/${token}.log" | grep -a "invalid dropping" | grep -a "DTS" | tail -3)
+    echo ===============${crash}
+    if [ "${crash}" != "" ]; then
         bash ${rootdir}/${token}.sh bg 750
-        sleep 15
-    fi    
-    log=$(cat "${rootdir}/log/${token}.log" | grep -a "frame=" | grep -a "bitrate=" | grep -a "time=" | grep -a "size=" | grep -a "fps=" | head -3)
-    echo --------------$log
-    if [ "${log}" = "" ]; then
-        bash ${rootdir}/${token}.sh bg 750
-        sleep 15
     fi
-    sleep 5
+    sleep 20
+
+    frame_log=$(cat "${rootdir}/log/${token}.log" | grep -a "frame=" | grep -a "bitrate=" | grep -a "time=" | grep -a "size=" | grep -a "fps=" | tail -3)
+    echo --------------${frame_log}
+    if [ "${frame_log}" = "" ]; then
+        bash ${rootdir}/${token}.sh bg 750
+    fi
+    if [ "${frame_log}" != "" ] && [ "${prev_frame_log}" = "${frame_log}" ]; then
+        bash ${rootdir}/${token}.sh bg 750
+    fi
+    prev_frame_log=${frame_log}
+    sleep 20
 done
