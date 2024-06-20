222e861f3460b9dbe13d03746c7e95da3e9c1fba
Dongxing Zhang <dongzhan@qti.qualcomm.com>
2024-06-20 17:07:17 +0800

    增加simpletv
    Signed-off-by: Dongxing Zhang <dongzhan@qti.qualcomm.com>

---
+++ SimpleTV/app/src/main/java/com/ww/simpletv/Constant.kt
@@ -0,0 +1,25 @@
+package com.ww.simpletv
+
+/**
+ *
+ * Copyright (C), 2024 Potato-66, All rights reserved.
+ * 创建时间: 2024/2/26
+ * @since 1.0
+ * @version 1.0
+ * @author Potato-66
+ */
+object Constant {
+    const val FILE_NAME = "IPTV.m3u"
+    const val KEY_LAST_CHANNEL = "last_channel"
+    const val KEY_BOOT_STARTUP = "boot_startup"
+    const val KEY_AUTO_UPDATE = "auto_update"
+    const val KEY_INIT = "init"
+    const val KEY_FONT_SIZE = "font_size"
+    const val DIALOG_TAG_SETTING = "setting"
+    const val DIALOG_TAG_CHANNEL = "channel"
+    const val DIALOG_TAG_UPDATE = "update"
+    const val APK_NAME = "SimpleTV.apk"
+    const val FONT_SIZE_NORMAL = 1.0f
+    const val FONT_SIZE_LARGE = 1.2f
+    const val FONT_SIZE_HUGE = 1.4f
+}
