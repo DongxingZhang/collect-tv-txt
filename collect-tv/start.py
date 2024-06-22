2d173458dbc586dba89e7ee79909ee39d0e133e3
Your Name <you@example.com>
2024-06-22 23:21:31 +0800

    20240622232131
    Signed-off-by: Your Name <you@example.com>

--- collect-tv/start.py
+++ collect-tv/start.py
@@ -1,5 +1,14 @@
 from main import refresh
-
+import os
+from datetime import datetime
+import time
 while True:
-    refresh("init", None)
-    refresh("epgpw", "download")
+    #refresh("init", None)
+    #refresh("epgpw", "download")
+    os.system("git add history")
+    message=datetime.now().strftime('%Y%m%d%H%M%S')
+    os.system("git commit -a -s -m '" + message + "'")
+    os.system("git push")
+    time.sleep(20)
+    break
+
